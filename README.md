5d078083daedd0a9245d5003df58d2a7e249b661
Daniel Gross <dan.12sq@gmail.com>
2017-10-29 15:20:02 -0400

    cleaned up README, turned into a class

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # TinderAutoSwipe_Selenium
+
 Script set up to automatically swipe right on tinder.com
 
-INSTALL
-This app utilizes selenium and python 3 and Chrome
-Python installation can be found here:
+## Installation/Requirements
+
+This app utilizes Python3, Selenium, and Chrome.
+
+Python3 can be found here:
 https://www.python.org/downloads/
 
 Selenium can be found here:
 http://www.seleniumhq.org/download/
 
-A chrome webdriver will be required with selenium. This can be found here:
+A Chrome webdriver will be required with Selenium. This can be found here:
 https://sites.google.com/a/chromium.org/chromedriver/downloads
 
-USE:
+## Usage
+
 The code is set up to first start up facebook and allow user input for your facebook username and password.
 After a sign in then it will go to tinder and attempt a sign in. Which should sign in if the facebook sign in worked.
 Once signed into tinder since selenium starts up a testing environment we have to go through the tutorial every time.
--- tinder.py
+++ tinder.py
@@ -1,50 +1,68 @@
+#!/usr/bin/env python3
+
 import time
+
+from getpass import getpass
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get('https://www.facebook.com/')
-print("Opened facebook. Please enter username")
-a = driver.find_element_by_id('email')
-a.send_keys(input())
-print("Email Id entered. Please enter password")
-b = driver.find_element_by_id('pass')
-b.send_keys(input())
-print("Password entered...")
-c = driver.find_element_by_id('loginbutton')
-c.click()
-driver.get('http://tinder.com')
-print("Facebook Login complete. lets go to tinder")
-time.sleep(5)
-print("Clicking on sign into facebook")
-driver.find_element_by_xpath("//*[@id=\"modal-manager\"]/div/div/div[2]/div[1]/div/div[3]/button[1]/span").click();
-time.sleep(2)
-try:
-    print("Clicking on tutorial prompts")
-    driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div[1]/div[1]/div/button/span/span").click()
-    print("Prompt 1")
-    time.sleep(2)
-    driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/main/div/button/span/span").click()
-    print("Prompt 2")
-    time.sleep(2)
-    driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]/span/span").click()
-    print("Prompt 3")
-    time.sleep(2)
-    driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]/span/span").click()
-    print ("Prompt 4")
-except:
-    pass
-print("Should be ready to start swiping")
-actions = ActionChains(driver)
-time.sleep(5)
-print("Swipe till there are no more profiles")
-try:
-    #Stop swiping by catching the exception of not finding a profile. closes browser
-    while driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[1]"):
-        time.sleep(2)
-        actions.send_keys(Keys.ARROW_RIGHT).perform()
-except:
-    print("No more profile found. Quitting")
-    driver.quit()
+class Swiper():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
 
+    def fb_login(self):
+        self.driver.get('https://www.facebook.com/')
+        a = self.driver.find_element_by_id('email')
+        a.send_keys(input('Opened Facebook. Please enter username/email: '))
+        b = self.driver.find_element_by_id('pass')
+        b.send_keys(getpass(prompt='Username entered. Please enter password: '))
+        print("Password entered. Logging in.")
+        c = self.driver.find_element_by_id('loginbutton')
+        c.click()
+        # TODO add a check to ensure FB login worked
+        return True
+
+    def tinder_login(self):
+        self.driver.get('http://tinder.com')
+        time.sleep(5)
+        print("Clicking on sign in with Facebook.")
+        self.driver.find_element_by_xpath("//*[@id=\"modal-manager\"]/div/div/div[2]/div[1]/div/div[3]/button[1]/span").click();
+        time.sleep(2)
+        try:
+            print("Dismissing tutorial prompts")
+            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div[1]/div[1]/div/button/span/span").click()
+            print("Prompt 1")
+            time.sleep(2)
+            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/main/div/button/span/span").click()
+            print("Prompt 2")
+            time.sleep(2)
+            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]/span/span").click()
+            print("Prompt 3")
+            time.sleep(2)
+            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]/span/span").click()
+            print ("Prompt 4")
+        except:
+            print('Something went wrong during login.')
+            return False
+        print("Ready to start swiping.")
+        return True
+
+    def swipe_tinder(self):
+        actions = ActionChains(self.driver)
+        time.sleep(5)
+        print("Swipe until there are no more profiles.")
+        try:
+            #Stop swiping by catching the exception of not finding a profile. closes browser
+            while self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[1]"):
+                time.sleep(2)
+                actions.send_keys(Keys.ARROW_RIGHT).perform()
+        except:
+            print("No more profiles found. Quitting.")
+            self.driver.quit()
+
+if __name__ == "__main__":
+    swiper = Swiper()
+    swiper.fb_login()
+    if swiper.tinder_login():
+        swiper.swipe_tinder()

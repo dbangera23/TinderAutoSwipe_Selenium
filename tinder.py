866f64e813d924b7646d3b9019a553d3c9c68b8b
pony <0netrickp0ny0203@example.com>
2019-02-28 17:47:21 +0900

    Update tinder.py

--- tinder.py
+++ tinder.py
@@ -7,20 +7,21 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
+
 class Swiper():
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome("./chromedriver")
 
     def fb_login(self):
         self.driver.get('https://www.facebook.com/')
-        a = self.driver.find_element_by_id('email')
-        a.send_keys(input('Opened Facebook. Please enter username/email: '))
-        b = self.driver.find_element_by_id('pass')
-        b.send_keys(getpass(prompt='Username entered. Please enter password: '))
+        a = self.driver.find_element_by_id("0netrickp0ny0203@gmail.com")
+        a.send_keys("")
+        b = self.driver.find_element_by_id("741986532ffr")
+        b.send_keys("")
         print("Password entered. Logging in.")
         c = self.driver.find_element_by_id('loginbutton')
         c.click()
-        try: #Check whether login was successful by finding the home button
+        try:  # Check whether login was successful by finding the home button
             self.driver.find_element_by_id('u_0_c')
         except:
             return False
@@ -29,22 +30,23 @@ class Swiper():
     def tinder_login(self):
         self.driver.get('http://tinder.com')
         time.sleep(5)
+#        self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div/button/span/span").click()
+#        print("done")
+#        time.sleep(2)
         print("Clicking on sign in with Facebook.")
-        self.driver.find_element_by_xpath("//*[@id=\"modal-manager\"]/div/div/div[2]/div[1]/div/div[3]/button[1]/span").click();
+        self.driver.find_element_by_xpath("//*[@id=\"modal-manager\"]/div/div/div[2]/div/div[3]/div[1]/button/span/span").click()
+        print("done")
         time.sleep(2)
-        try: #Selenium scripts open a testing environment in chrome. Every login acts like a brand new login. Must click through tutorial
+        print("Prompt 0")
+        try:  # Selenium scripts open a testing environment in chrome. Every login acts like a brand new login. Must click through tutorial
             print("Dismissing tutorial prompts")
-            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div[1]/div[1]/div/button/span/span").click()
+            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/div[3]/button[1]/span/span").click()
             print("Prompt 1")
             time.sleep(2)
-            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/main/div/button/span/span").click()
+            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/div[3]/button[1]/span/span").click()
             print("Prompt 2")
-            time.sleep(2)
-            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]/span/span").click()
-            print("Prompt 3")
-            time.sleep(2)
-            self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[2]/div/div/div[1]/div/div/div[4]/button[1]/span/span").click()
-            print ("Prompt 4")
+            print("Ready to start swiping.")
+            return True
         except:
             print('Something went wrong during login.')
             return False
@@ -53,16 +55,22 @@ class Swiper():
 
     def swipe_tinder(self):
         actions = ActionChains(self.driver)
-        time.sleep(5)
+        time.sleep(30)
         print("Swipe until there are no more profiles.")
         try:
-            #Stop swiping by catching the exception of not finding a profile. closes browser
+            # Stop swiping by catching the exception of not finding a profile. closes browser
             while self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[1]"):
                 actions.send_keys(Keys.ARROW_RIGHT).perform()
                 time.sleep(2)
         except:
-            print("No more profiles found. Quitting.")
-            self.driver.quit()
+            time.sleep(20)
+            try:
+                while self.driver.find_element_by_xpath("//*[@id=\"content\"]/div/span/div/div[1]/div/main/div/div/div/div[1]/div[1]/div/div[3]/div[1]"):
+                    actions.send_keys(Keys.ARROW_RIGHT).perform()
+                    time.sleep(2)
+            except:
+                print("No more profiles found. Quitting.")
+                self.driver.quit()
 
 if __name__ == "__main__":
     swiper = Swiper()
